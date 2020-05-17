59fc81528665ddd4523ccb02479f2015e7282727
vasu vanka <vanka.vasu@gmail.com>
2020-05-17 09:18:49 +0530

    converted const variable to func

--- mod.ts
+++ mod.ts
@@ -5,11 +5,9 @@ const random = (bytes: number) => {
   return crypto.getRandomValues(new Uint32Array(bytes));
 }
 // uniq - will generate unique ids in javascript
-export const uniq = (size: number = 12): string => {
+export default function uniq(size: number = 12): string {
   let id: string = "";
   const bytes: Uint32Array = random(size);
   while (size--) id += url[bytes[size] & 63];
   return id;
 };
-
-export default uniq;
